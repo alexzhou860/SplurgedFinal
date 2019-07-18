6a00f781b3012d20ef97eba2231eb32170f89556
Alex Zhou <zhoua2021@gmail.com>
2019-07-18 17:54:24 -0500

    finished all three items for the last page

--- Splurged/EntertainmentDetailsViewController.swift
+++ Splurged/EntertainmentDetailsViewController.swift
@@ -9,20 +9,30 @@
 import UIKit
 import MapKit
 import SafariServices
+import CoreLocation
 
-class EntertainmentDetailsViewController: UIViewController {
+class EntertainmentDetailsViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var nameLabel: UILabel!
-   @IBOutlet weak var addressLabel: UILabel!
- 
-    
-
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var entertainmentDetailsMapView: MKMapView!
     
     var selectedMapItem = MKMapItem()
+    let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
+    var locations: [CLLocation] = []
+    let annotation = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(selectedMapItem.name!)
+        annotation.coordinate = selectedMapItem.placemark.coordinate
+        entertainmentDetailsMapView.addAnnotation(annotation)
+        locationManager.requestWhenInUseAuthorization()
+        entertainmentDetailsMapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        entertainmentDetailsMapView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +43,29 @@ class EntertainmentDetailsViewController: UIViewController {
         address += selectedMapItem.placemark.administrativeArea! + " "
         address += selectedMapItem.placemark.postalCode!
         addressLabel.text = address
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+        region = MKCoordinateRegion(center: center, span: span)
+        entertainmentDetailsMapView.setRegion(region, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
     }
     
     @IBAction func phoneCallButton(_ sender: Any) {
